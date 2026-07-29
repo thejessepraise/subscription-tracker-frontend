b35b5a9ff3e90a559163cb114ecb847a3ab845df
thejessepraise <jesseyankson86@gmail.com>
2026-07-29 17:11:05 +0000

    updated final

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // FASTAPI URL
 // ==========================================
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://subscription-tracker-backend-tspm.onrender.com";
 
 
 // ==========================================
